eceba66371df1d83a9e4ff72354a01daa96b6ad4
keymus <bilbo2012@bk.ru>
2021-02-20 14:48:08 +0300

    first workable version

--- src/test/java/com/example/security/SecurityApplicationTests.java
+++ src/test/java/com/example/security/SecurityApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.security;
 
+import com.example.security.controllers.PhoneController;
+import com.example.security.domain.Phone;
+import com.example.security.repos.PhoneRepo;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Testcontainers
 @SpringBootTest
 class SecurityApplicationTests {
 
+    @Container
+    public static PostgreSQLContainer container = new PostgreSQLContainer()
+            .withDatabaseName("test")
+            .withPassword("test")
+            .withUsername("test");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.username", container::getUsername);
+    }
+
+    @Autowired
+    private PhoneController phoneController;
+
     @Test
     void contextLoads() {
+        Phone phone = phoneController.addPhone(new Phone());
+        assertNotNull(phone.getId());
     }
 
 }
