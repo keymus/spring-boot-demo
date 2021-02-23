b0160b479c969f351b738aad9d12b51a54f4c550
keymus <bilbo2012@bk.ru>
2021-02-23 17:57:53 +0300

    first commit

--- src/main/java/com/example/security/repos/MessageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.security.repos;
-
-import com.example.security.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepo extends JpaRepository<Message, Long> {
-}
